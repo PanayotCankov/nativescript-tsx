2fe3d425144060dd549bb05dbcbb2a0de81e34ca
Panayot Cankov <panayot.cankov@telerik.com>
2017-10-30 15:40:51 +0200

    Update to 3.*

--- nativescript-tsx/builder.ts
+++ nativescript-tsx/builder.ts
@@ -1,30 +1,33 @@
-import {getBindingOptions, bindingConstants} from "ui/builder/binding-builder";
-import {View, isEventOrGesture} from "ui/core/view";
-import {Property} from "ui/core/dependency-observable";
+import { getBindingOptions, bindingConstants } from "ui/builder/binding-builder";
+import { View } from "ui/core/view";
+import { isEventOrGesture } from "ui/core/bindable";
+import { GestureTypes } from "ui/gestures/gestures";
+import { Property } from "ui/core/properties";
 
 class Setter {
-    constructor(public property: Property, public value: any) {
+    constructor(public property: Property<any, any>, public value: any) {
     }
 }
 
 export class UIBuilder {
-    static createElement(ctor: { new(): any, name: string } | Property, attributes: {}, ... content: any[]): any {
+    static createElement(ctor: { new(): any, name: string } | Property<any, any>, attributes: {}, ... content: any[]): any {
         if (ctor instanceof Property) {
-            // Complex property. Wild guess - ListView.itemTemplate. Provision type introspection.
+            // TODO: The new property system (post 3.0) defines the properties in the modules instead of on the class.
+            // Maybe property.register(class) should assign the property on the class somehow,
+            // so complex properties can be checked when building from templates.
             if (content.length != 1) {
                 throw new Error("Expected exactly one item for template");
             }
             return new Setter(ctor, content[0]);
         } else {
             let view = new ctor();
-            // console.log("new " + ctor.name + "()");
 
             for(let key in attributes) {
                 let value = attributes[key];
 
-                // Because why not?
                 if (key === "class") {
-                    key = "cssClass";
+                    // I regret nothing!
+                    key = "className";
                 }
 
                 if (isString(value) && isBinding(attributes[key])) {
@@ -39,9 +42,18 @@ export class UIBuilder {
                     continue;
                 }
 
-                if (isEventOrGesture(key, view) && typeof value === "function") {
-                    // console.log(" - event or gesture " + key + " " + value);
-                    view.on(key, value);
+                var isev = isEventOrGesture(key, view);
+                var ison = key.startsWith("on");
+
+                if ((isev || ison) && typeof value === "function") {
+                    var mapped = key;
+                    if (ison && key.length > 2) {
+                        mapped = mapped[2].toLowerCase() + mapped.substr(3);
+                    }
+                    if (mapped in GestureTypes) {
+                        mapped = GestureTypes[mapped];
+                    }
+                    view.on(mapped, value);
                     continue;
                 }
 
